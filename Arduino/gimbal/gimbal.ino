0b5c9d9862c39fabc9fe25359a714e64e4bb18fb
heklahelgadottir <heklahelgadottir@gmail.com>
2016-04-20 16:16:16 +0100

    Range changed to -90 to 90

--- Arduino/gimbal/gimbal.ino
+++ Arduino/gimbal/gimbal.ino
@@ -5,26 +5,35 @@ Servo yAxis;
 
 int pitch = 10;
 int yaw = 10;
-int xLower = 20;
-int xUpper = 170;
-int yLower = 10;
-int yUpper = 180;
+int xLower = 45;
+int xUpper = 140;
+int yLower = 0;
+int yUpper = 140;
 
 void setup() {
-  yAxis.attach(5);
-  xAxis.attach(6);
+  Serial.begin(9600);
+  yAxis.attach(6);
+  xAxis.attach(5);
 }
 
 int degreeChange(int degree, int lowerLimit, int upperLimit) {
   int range = upperLimit-lowerLimit;           // Calculates the range of the degrees available on the gimbal
   double proportion = range / 180.00;          // Calculates the proportion of actual range compared to projected range
-  double actualDegree = degree * proportion + lowerLimit;     // Calculates what angle to give as an instruction so that the
+  double actualDegree = (degree + 90) * proportion + lowerLimit;     // Calculates what angle to give as an instruction so that the
+  Serial.print("degree proportion: ");
+  Serial.println(actualDegree);
   return actualDegree;                                        // gimbal turns the right angle       
 }
 
+int yCorrection(int degree) {
+    //Serial.println((degree - 90) * (-1));
+    return ((degree - 90) * (-1));  // Takes in an int between -90 and 90, returns a value between 0 and 180
+}
+
 void loop() {  
-  xAxis.write(degreeChange(pitch, xLower, xUpper));
-  yAxis.write(degreeChange(yaw, yLower, yUpper));
+  xAxis.write(degreeChange(yaw, xLower, xUpper));
+  yAxis.write(yCorrection(pitch));
+  //Serial.println(yCorrection(pitch));
   pitch = pitch + 5;
   yaw = yaw + 5;
   delay(1000);
