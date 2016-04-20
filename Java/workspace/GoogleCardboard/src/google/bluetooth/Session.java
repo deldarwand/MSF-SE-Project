a6b40be005fb727b62c1353d9f361eef98ef938e
BuildTools <glsm.channel@gmail.com>
2016-04-20 23:17:50 +0100

    Edit to Session.java

--- Java/workspace/GoogleCardboard/src/google/bluetooth/Session.java
+++ Java/workspace/GoogleCardboard/src/google/bluetooth/Session.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import javax.imageio.ImageIO;
 import javax.microedition.io.StreamConnection;
 
 import com.fazecast.jSerialComm.SerialPort;
